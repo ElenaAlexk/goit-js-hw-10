835fe68b0ece4abd08beb89ee4ba71811f65bbd4
Elena Kvasko <lenakvasko@gmail.com>
2023-02-08 21:18:39 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import './css/styles.css';
-
+import { fetchCountries } from './js/fetchCountries';
 const DEBOUNCE_DELAY = 300;
 
-fetch('https://restcountries.com/v3.1/name/{name}')
-  .then(response => {
-    return response.json();
-  })
-  .then(name => {
-    console.log();
-  })
-  .catch(error => {
-    console.log(error);
-  });
+const inputRef = document.querySelector('#search-box');
+const listRef = document.querySelector('country-list');
+const infoRef = document.querySelector('country-info');
+
+inputRef.addEventListener('input', onSearch);
+function onSearch(event) {
+  event.preventDefault;
+  const inputCountries = event.target.value.trim();
+  if (inputCountries) {
+    return fetchCountries(inputCountries)
+      .then(result => {})
+      .catch(error => {});
+    //.finally(() => inputCountry.reset);
+  }
+}
+
+function countryListMarkup() {}
+function countryCardMarkup() {}
