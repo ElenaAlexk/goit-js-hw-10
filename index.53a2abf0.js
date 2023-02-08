6a875bb12680da8248cfa54282b90401adb06e26
ElenaAlexk <111981718+ElenaAlexk@users.noreply.github.com>
2023-02-08 19:19:05 +0000

    Deploying to gh-pages from @ ElenaAlexk/goit-js-hw-10@835fe68b0ece4abd08beb89ee4ba71811f65bbd4 🚀

--- index.53a2abf0.js
+++
@@ -1,2 +0,0 @@
-fetch("https://restcountries.com/v3.1/name/{name}").then((function(n){return n.json()})).then((function(n){console.log()})).catch((function(n){console.log(n)}));
-//# sourceMappingURL=index.53a2abf0.js.map
--- index.64b0cdee.js
+++
@@ -1,2 +0,0 @@
-fetch("https://restcountries.com/v3.1/name/{name}").then((e=>e.json())).then((e=>{console.log()})).catch((e=>{console.log(e)}));
-//# sourceMappingURL=index.64b0cdee.js.map
